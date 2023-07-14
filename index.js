57eeb369d2f8dfd1e7a2ec3b3d62139d268b1c1b
allioneandrea <allioneandrea@gmail.com>
2023-07-14 12:12:17 +0200

    ultimo commit

--- index.js
+++ index.js
@@ -9,8 +9,26 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+const db = mysql.createConnection({
+    host: "bu4o4mfzvbviqqt3abkj-mysql.services.clever-cloud.com",
+    user: "u28o1t7bqutx2zvj",
+    password: "i9GAaE9mfjcEK3QFz9A4",
+    database: "bu4o4mfzvbviqqt3abkj"
+
+})
+
 app.get("/", (req,res) => {
     res.send({"inicio": "inicio"})
+})
+
+
+app.get('/api/horario', (req,res) => {
+
+    db.query("SELECT * FROM horario ORDER BY inicio ASC", (err, result) => {
+        res.send(result)
+    })
+    
 })
 
 app.listen(PORT, () => {
